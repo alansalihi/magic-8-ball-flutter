67092417561074b26141200131b03d6632458f2f
Alan Salihi <alansalihi@gmail.com>
2019-12-01 22:40:45 +0100

    built v 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Center(child: Text('Ask Me Anything')),
+            backgroundColor: Colors.indigo,
+          ),
+          body: magic8Page(),
+        ),
       ),
     );
+
+class magic8Page extends StatefulWidget {
+  @override
+  _magic8PageState createState() => _magic8PageState();
+}
+
+class _magic8PageState extends State<magic8Page> {
+  int answer = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  answer = Random().nextInt(5) + 1;
+                  print('ball is tapped! ball is: $answer');
+                });
+              },
+              child: Image.asset('images/ball$answer.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
